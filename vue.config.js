ab9ac502a4d29aa0c2c3303e2f69f9ee2745a487
yonabe710 <at0arumati@gmail.com>
2020-04-24 09:00:57 +0900

    scss反映成功

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
     // アプリケーション全体に関わるcssの設定
     css: {
       loaderOptions: {
-        sass: {
-          data: `@import "@/public/scss/style.scss";`
+        scss: {
+          prependData: `@import "@/assets/scss/style.scss";`
         }
       }
     }
