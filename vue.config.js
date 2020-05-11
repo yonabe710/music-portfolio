4775c3e38a5aa097c6aaf243ae0f64a63fad5e95
imataka7 <imataka7@gmail.com>
2020-05-12 00:57:39 +0900

    Fuck off vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    // アプリケーション全体に関わるcssの設定
-    css: {
-      loaderOptions: {
-        scss: {
-          prependData: `@import "@/assets/scss/style.scss";`
-        }
-      }
-    }
-  }
+  // アプリケーション全体に関わるcssの設定
+  // css: {
+  //   loaderOptions: {
+  //     scss: {
+  //       prependData: `@import "@/assets/scss/style.scss";`
+  //     }
+  //   }
+  // }
+};
